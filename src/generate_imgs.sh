7f8ecbd6d6c4c1550f29b14742c3b3f09b9087fb
Hikmat Farhat <hfarhat@ndu.edu.lb>
2020-10-22 08:35:23 +0300

    update

--- src/generate_imgs.sh
+++ src/generate_imgs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake ../src/ -DBOOST='/mnt/c/Program Files/Boost/boost_1_74_0/boost_1_74_0'
+cmake ../src/ -DBoost_INCLUDE_DIR=$BOOST_ROOT_1_72_0/include
+#cmake ../src/ -DBoost_INCLUDE_DIR='/mnt/c/Program Files/Boost/boost_1_74_0/boost_1_74_0'
 make
 
 for x in $(ls ../*.desc)
